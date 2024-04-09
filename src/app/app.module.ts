51c8a735d3ae981d7303f0ed2827477ae8d491bb
VIS-WAS <vishwas.ms8830@gmail.com>
2024-04-09 18:48:36 +0530

    creating custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { PercentagePipe } from './Pipes/percentage.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdminComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AdminComponent, PercentagePipe],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
